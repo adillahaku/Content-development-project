8db5401c7bd302cb5c860da9f9d3dd9be5c35cd7
Auw109 <auwalulfjr15@gmail.com>
2024-10-18 09:55:38 +0700

    timeerrr

--- Assets/GolemiteGames/TimersMadeEasyLite/Scripts/DemoFunctionsForEvents.cs
+++ Assets/GolemiteGames/TimersMadeEasyLite/Scripts/DemoFunctionsForEvents.cs
@@ -3,17 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+
 public class DemoFunctionsForEvents : MonoBehaviour
 {
-    public GameObject finishedText;
-
-    public Timer timer;
+    public GameObject finishedText; // Teks yang muncul saat timer selesai
+    public Timer timer; // Referensi ke skrip Timer
+    
+    // Fungsi ini dipanggil saat timer dimulai
     public void TimerStart()
     {
+        // Menyembunyikan teks yang menandakan timer telah selesai
         finishedText.SetActive(false);
+        
+        // Memulai timer
+        timer.StartTimer();
     }
+
+    // Fungsi ini dipanggil saat timer berakhir
     public void TimerEnd()
     {
+        // Menampilkan teks yang menandakan timer telah selesai
         finishedText.SetActive(true);
     }
+
 }
